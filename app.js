df8849bd52c97d0ce3cb0b3afed6ef4c3054b959
mhmdhalawi <mhmdhalawi@users.noreply.github.com>
2020-02-02 03:03:30 +0200

    updated nav

--- app.js
+++
@@ -1,4 +0,0 @@
-const body = document.querySelector('body');
-const button = document.querySelector('.navigation__button');
-
-button.addEventListener('click', () => body.classList.toggle('hideBar'));
